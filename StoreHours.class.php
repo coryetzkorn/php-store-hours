6e65434d146abb440b4a80b56c813adeec72c875
Marc Ermshaus <marc@ermshaus.org>
2016-02-13 15:01:22 +0100

    Add unit tests. Improve code. Fix #7.

--- StoreHours.class.php
+++ StoreHours.class.php
@@ -1,126 +1,228 @@
-<?php 
+<?php
 
-// ----------------------------------
-// PHP STORE HOURS
-// ----------------------------------
-// Version 3.0
-// Written by Cory Etzkorn
-// https://github.com/coryetzkorn/php-store-hours
+/**
+ * ----------------------------------
+ * PHP STORE HOURS
+ * ----------------------------------
+ * Version 3.0
+ * Written by Cory Etzkorn
+ * https://github.com/coryetzkorn/php-store-hours
+ *
+ * DO NOT MODIFY THIS CLASS FILE
+ */
+class StoreHours
+{
+    /**
+     *
+     * @var array
+     */
+    private $hours;
 
-// DO NOT MODIFY THIS CLASS FILE
+    /**
+     *
+     * @var array
+     */
+    private $exceptions;
 
-class StoreHours {
+    /**
+     *
+     * @var array
+     */
+    private $templates;
 
-  function __construct($hours = array(), $exceptions = array(), $template = array()) {
-    $this->hours = $hours;
-    $this->exceptions = $exceptions;
-    $this->template = $template;
-    if(!isset($this->template['open'])) {
-      $this->template['open'] = "<h3>Yes, we're open! Today's hours are {%hours%}.</h3>";
-    }
-    if(!isset($this->template['closed'])) {
-      $this->template['closed'] = "<h3>Sorry, we're closed. Today's hours are {%hours%}.</h3>";
-    }
-    if(!isset($this->template['closed_all_day'])) {
-      $this->template['closed_all_day'] = "<h3>Sorry, we're closed.</h3>";
-    }
-    if(!isset($this->template['separator'])) {
-      $this->template['separator'] = " - ";
-    }
-    if(!isset($this->template['join'])) {
-      $this->template['join'] = " and ";
-    }
-    if(!isset($this->template['format'])) {
-      $this->template['format'] = "g:ia";
-    }
-    if(!isset($this->template['hours'])) {
-      $this->template['hours'] = "{%open%}{%separator%}{%closed%}";
-    }
-  }
+    /**
+     *
+     * @var boolean
+     */
+    private $yesterdayFlag;
 
-  // Returns today's hours
-  public function hours_today() {
-    $today = strtotime('today midnight');
-    $day = strtolower(date("D"));
-    $exceptions = $this->exceptions;
-    $hours_today = $this->hours[$day];
-    if($exceptions) {
-      foreach($exceptions as $ex_day => $ex_hours) {
-        //echo 'ex' . date('r', strtotime($ex_day + '/2014'));
-        //echo 'today' . date('r', $today);
-        if(strtotime($ex_day) == $today) {
-          // Today is an exception, use alternate hours instead
-          $hours_today = $ex_hours;
+    /**
+     *
+     * @param array $hours
+     * @param array $exceptions
+     * @param array $templates
+     */
+    public function __construct($hours = array(), $exceptions = array(), $templates = array())
+    {
+        $this->hours         = $hours;
+        $this->exceptions    = $exceptions;
+        $this->templates     = $templates;
+        $this->yesterdayFlag = false;
+
+        // Remove empty elements from values (backwards compatibility)
+        foreach ($this->hours as $key => $value) {
+            $this->hours[$key] = array_filter($value, function ($element) {
+                return (trim($element) !== '');
+            });
         }
-      }
-    }
-    return $hours_today;
-  }
 
-  // Returns boolean
-  public function is_open() {
-    $now = strtotime(date("G:i"));
-    $hours_today = $this->hours_today();
-    $exceptions = $this->exceptions;
-    $is_open = 0;
-    if(!empty($hours_today[0])) {
-      foreach($hours_today as $range) {
-        $range = explode("-", $range);
-        $start = strtotime($range[0]);
-        $end = strtotime($range[1]);
-        // Add one day if the end time is past midnight
-        if($end <= $start) {
-          $end = strtotime($range[1] . ' + 1 day');
+        // Remove empty elements from values (backwards compatibility)
+        foreach ($this->exceptions as $key => $value) {
+            $this->exceptions[$key] = array_filter($value, function ($element) {
+                return (trim($element) !== '');
+            });
         }
-        if (($start <= $now) && ($end >= $now)) {
-          $is_open ++;
+
+        $defaultTemplates = array(
+            'open'           => '<h3>Yes, we\'re open! Today\'s hours are {%hours%}.</h3>',
+            'closed'         => '<h3>Sorry, we\'re closed. Today\'s hours are {%hours%}.</h3>',
+            'closed_all_day' => '<h3>Sorry, we\'re closed.</h3>',
+            'separator'      => ' - ',
+            'join'           => ' and ',
+            'format'         => 'g:ia',
+            'hours'          => '{%open%}{%separator%}{%closed%}'
+        );
+
+        $this->templates += $defaultTemplates;
+    }
+
+    /**
+     *
+     * @param string $timestamp
+     * @return array Today's hours
+     */
+    public function hours_today($timestamp = null)
+    {
+        $timestamp     = (null !== $timestamp) ? $timestamp : time();
+        $today         = strtotime(date('Y-m-d', $timestamp) . ' midnight');
+        $weekday_short = strtolower(date('D', $timestamp));
+
+        $hours_today = array();
+
+        if (isset($this->hours[$weekday_short])) {
+            $hours_today = $this->hours[$weekday_short];
         }
-      }
-    }
-    if($is_open > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
-  // Prep HTML
-  private function render_html($template_name) {
-    $template = $this->template;
-    $hours_today = $this->hours_today();
-    $output = '';
-    $index = 0;
-    if(!empty($hours_today[0])) {
-      $hours_template = '';
-      foreach($hours_today as $range) {
-        $range = explode("-", $range);
-        $start = strtotime($range[0]);
-        $end = strtotime($range[1]);
-        if($index >= 1) {
-          $hours_template .= $template['join'];
+        foreach ($this->exceptions as $ex_day => $ex_hours) {
+            if (strtotime($ex_day) === $today) {
+                // Today is an exception, use alternate hours instead
+                $hours_today = $ex_hours;
+            }
         }
-        $hours_template .= $template['hours'];
-        $hours_template = str_replace('{%open%}', date($template['format'], $start), $hours_template);
-        $hours_template = str_replace('{%closed%}', date($template['format'], $end), $hours_template);
-        $hours_template = str_replace('{%separator%}', $template['separator'], $hours_template);
-        $index ++;
-      }
-      $output .= str_replace('{%hours%}', $hours_template, $template[$template_name]);
-    } else {
-      $output .= $template['closed_all_day'];
-    }
-    echo $output;
-  }
 
-  // Output HTML
-  public function render() {
-    if($this->is_open()) {
-      $this->render_html('open');
-    } else {
-      $this->render_html('closed');
+        return $hours_today;
     }
-  }
 
+    /**
+     *
+     * @param string $timestamp
+     * @return boolean
+     */
+    public function is_open($timestamp = null)
+    {
+        $timestamp = (null !== $timestamp) ? $timestamp : time();
+
+        $is_open = false;
+        $this->yesterdayFlag = false;
+
+        // Check whether shop's still open from day before
+
+        $ts_yesterday    = strtotime(date('Y-m-d H:i:s', $timestamp) . ' -1 day');
+        $yesterday       = date('Y-m-d', $ts_yesterday);
+        $hours_yesterday = $this->hours_today($ts_yesterday);
+
+        foreach ($hours_yesterday as $range) {
+            $range = explode('-', $range);
+            $start = strtotime($yesterday . ' ' . $range[0]);
+            $end   = strtotime($yesterday . ' ' . $range[1]);
+
+            if ($end <= $start) {
+                $end = strtotime($yesterday . ' ' . $range[1] . ' +1 day');
+            }
+
+            if (($start <= $timestamp) && ($timestamp <= $end)) {
+                $is_open = true;
+                $this->yesterdayFlag = true;
+                break;
+            }
+        }
+
+        // Check today's hours
+
+        if (!$is_open) {
+            $day         = date('Y-m-d', $timestamp);
+            $hours_today = $this->hours_today($timestamp);
+
+            foreach ($hours_today as $range) {
+                $range = explode('-', $range);
+                $start = strtotime($day . ' ' . $range[0]);
+                $end   = strtotime($day . ' ' . $range[1]);
+
+                if ($end <= $start) {
+                    $end = strtotime($day . ' ' . $range[1] . ' +1 day');
+                }
+
+                if (($start <= $timestamp) && ($timestamp <= $end)) {
+                    $is_open = true;
+                    break;
+                }
+            }
+        }
+
+        return $is_open;
+    }
+
+    /**
+     * Prep HTML
+     *
+     * @param string $template_name
+     * @param int $timestamp
+     */
+    private function render_html($template_name, $timestamp)
+    {
+        $template    = $this->templates;
+        $hours_today = $this->hours_today($timestamp);
+        $day         = date('Y-m-d', $timestamp);
+        $output      = '';
+
+        if (count($hours_today) > 0) {
+            $hours_template = '';
+            $first = true;
+
+            foreach ($hours_today as $range) {
+                $range = explode('-', $range);
+                $start = strtotime($day . ' ' . $range[0]);
+                $end   = strtotime($day . ' ' . $range[1]);
+
+                if (false === $first) {
+                    $hours_template .= $template['join'];
+                }
+
+                $hours_template .= $template['hours'];
+
+                $hours_template = str_replace('{%open%}', date($template['format'], $start), $hours_template);
+                $hours_template = str_replace('{%closed%}', date($template['format'], $end), $hours_template);
+                $hours_template = str_replace('{%separator%}', $template['separator'], $hours_template);
+
+                $first = false;
+            }
+
+            $output .= str_replace('{%hours%}', $hours_template, $template[$template_name]);
+        } else {
+            $output .= $template['closed_all_day'];
+        }
+
+        echo $output;
+    }
+
+    /**
+     * Output HTML
+     *
+     * @param string $timestamp
+     */
+    public function render($timestamp = null)
+    {
+        $timestamp = (null !== $timestamp) ? $timestamp : time();
+
+        if ($this->is_open($timestamp)) {
+            // Print yesterday's hours if shop's still open from day before
+            if ($this->yesterdayFlag) {
+                $timestamp = strtotime(date('Y-m-d H:i:s', $timestamp) . ' -1 day');
+            }
+
+            $this->render_html('open', $timestamp);
+        } else {
+            $this->render_html('closed', $timestamp);
+        }
+    }
 }
-
-?>
---
+++ StoreHoursTest.php
@@ -0,0 +1,214 @@
+<?php
+
+/**
+ *
+ */
+class StoreHoursTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    protected function setUp()
+    {
+        require_once __DIR__ . '/StoreHours.class.php';
+
+        date_default_timezone_set('UTC');
+    }
+
+    /**
+     *
+     * @return \StoreHours
+     */
+    private function instantiateWithDefaultData()
+    {
+        $hours = array(
+            'mon' => array('11:00-20:30'),
+            'tue' => array('11:00-13:00', '18:00-20:30'),
+            'wed' => array('11:00-20:30'),
+            'thu' => array('11:00-1:30'), // Open late
+            'fri' => array('11:00-20:30'),
+            'sat' => array('11:00-20:00'),
+            'sun' => array('') // Closed all day
+        );
+
+        $exceptions = array(
+            '2/24' => array('11:00-18:00'),
+            '10/18' => array('11:00-16:00', '18:00-20:30'),
+
+            '2016-02-01' => array('09:00-11:00')
+        );
+
+        return new StoreHours($hours, $exceptions);
+    }
+
+    /**
+     *
+     */
+    public function testHoursTodayMethod()
+    {
+        $sh = $this->instantiateWithDefaultData();
+
+        $this->assertEquals(array('11:00-20:30'), $sh->hours_today(strtotime('2016-02-08'))); // mon
+        $this->assertEquals(array('11:00-13:00', '18:00-20:30'), $sh->hours_today(strtotime('2016-02-09'))); // tue
+        $this->assertEquals(array('11:00-20:30'), $sh->hours_today(strtotime('2016-02-10'))); // wed
+        $this->assertEquals(array('11:00-1:30'), $sh->hours_today(strtotime('2016-02-11'))); // thu
+        $this->assertEquals(array('11:00-20:30'), $sh->hours_today(strtotime('2016-02-12'))); // fri
+        $this->assertEquals(array('11:00-20:00'), $sh->hours_today(strtotime('2016-02-13'))); // sat
+        $this->assertEquals(array(), $sh->hours_today(strtotime('2016-02-14'))); // sun
+
+        // Exceptions (dates, not the PHP kind)
+
+        $this->assertEquals(array('11:00-18:00'), $sh->hours_today(strtotime('2016-02-24')));
+        $this->assertEquals(array('11:00-16:00', '18:00-20:30'), $sh->hours_today(strtotime('2016-10-18')));
+
+        $this->assertEquals(array('09:00-11:00'), $sh->hours_today(strtotime('2016-02-01')));
+        $this->assertEquals(array('11:00-20:30'), $sh->hours_today(strtotime('2017-02-01'))); // wed
+    }
+
+    /**
+     *
+     */
+    public function testIsOpenMethod()
+    {
+        $sh = $this->instantiateWithDefaultData();
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-08 10:59:59'))); // mon
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-08 11:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-08 20:30:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-08 20:30:01')));
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-09 10:59:59'))); // tue
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-09 11:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-09 13:00:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-09 13:00:01')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-09 17:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-09 18:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-09 20:30:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-09 20:30:01')));
+
+        // "open late"
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-11 00:30:00'))); // thu
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-11 23:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-12 00:00:00'))); // fri
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-12 01:30:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-12 01:30:01')));
+
+        // Exceptions (dates, not the PHP kind)
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-24 10:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-24 11:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-24 18:00:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-24 18:00:01')));
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-10-18 10:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-10-18 11:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-10-18 16:00:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-10-18 16:00:01')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-10-18 17:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-10-18 18:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-10-18 20:30:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-10-18 20:30:01')));
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-01 08:59:59')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-01 09:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2016-02-01 11:00:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2016-02-01 11:00:01')));
+
+        $this->assertEquals(false, $sh->is_open(strtotime('2017-02-01 10:59:59'))); // wed
+        $this->assertEquals(true, $sh->is_open(strtotime('2017-02-01 11:00:00')));
+        $this->assertEquals(true, $sh->is_open(strtotime('2017-02-01 20:30:00')));
+        $this->assertEquals(false, $sh->is_open(strtotime('2017-02-01 20:30:01')));
+    }
+
+    /**
+     *
+     */
+    public function testRenderMethod()
+    {
+        $sh = $this->instantiateWithDefaultData();
+
+        ob_start();
+        $sh->render(strtotime('2016-02-13 14:30:00')); // sat
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 11:00am - 8:00pm.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-09 14:30:00')); // tue
+        $this->assertEquals('<h3>Sorry, we\'re closed. Today\'s hours are 11:00am - 1:00pm and 6:00pm - 8:30pm.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-14 12:00:00')); // sun
+        $this->assertEquals('<h3>Sorry, we\'re closed.</h3>', ob_get_clean());
+
+        // "open late" (if still open, display hours from yesterday)
+
+        ob_start();
+        $sh->render(strtotime('2016-02-11 23:59:59')); // night from thu->fri, thursday's hours
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 11:00am - 1:30am.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-12 00:30:00')); // night from thu->fri, thursday's hours
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 11:00am - 1:30am.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-12 01:30:01')); // closed on friday morning, friday's hours
+        $this->assertEquals('<h3>Sorry, we\'re closed. Today\'s hours are 11:00am - 8:30pm.</h3>', ob_get_clean());
+
+        // Exceptions (dates, not the PHP kind)
+
+        ob_start();
+        $sh->render(strtotime('2016-02-24 19:00:00'));
+        $this->assertEquals('<h3>Sorry, we\'re closed. Today\'s hours are 11:00am - 6:00pm.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-10-18 19:00:00'));
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 11:00am - 4:00pm and 6:00pm - 8:30pm.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-01 09:00:00'));
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 9:00am - 11:00am.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-01 12:00:00'));
+        $this->assertEquals('<h3>Sorry, we\'re closed. Today\'s hours are 9:00am - 11:00am.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2017-02-01 12:00:00')); // wed
+        $this->assertEquals('<h3>Yes, we\'re open! Today\'s hours are 11:00am - 8:30pm.</h3>', ob_get_clean());
+    }
+
+    /**
+     *
+     */
+    public function testWithCustomTemplates()
+    {
+        $hours = array(
+            'mon' => array('09:00-17:00', '17:30-18:00', '19:00-02:30'),
+            'thu' => array('17:45-18:00')
+        );
+
+        $exceptions = array(
+            '2016-02-15' => array()
+        );
+
+        $templates = array(
+            'open'      => 'Open. Hours {%hours%}.',
+            'separator' => '-',
+            'format'    => 'G.i'
+        );
+
+        $sh = new StoreHours($hours, $exceptions, $templates);
+
+        ob_start();
+        $sh->render(strtotime('2016-02-08 14:30:00')); // mon
+        $this->assertEquals('Open. Hours 9.00-17.00 and 17.30-18.00 and 19.00-2.30.', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-11 14:30:00')); // thu
+        $this->assertEquals('<h3>Sorry, we\'re closed. Today\'s hours are 17.45-18.00.</h3>', ob_get_clean());
+
+        ob_start();
+        $sh->render(strtotime('2016-02-15 14:30:00')); // mon
+        $this->assertEquals('<h3>Sorry, we\'re closed.</h3>', ob_get_clean());
+    }
+}
